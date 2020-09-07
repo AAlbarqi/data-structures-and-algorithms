e5e5bf226a3d60e2108b59e6fa3d0c656a40a665
Ashjan Albarqi <ashjanalbarqi2@gmail.com>
2020-09-07 09:42:10 +0300

    Update array-reverse.js

--- code-challenges/arrayReverse/array-reverse.js
+++ code-challenges/arrayReverse/array-reverse.js
@@ -5,3 +5,13 @@ let reverseArray = (arr) => {
     }
     return reversedArr;
 }
+//Another solution
+
+let reverseArrayInPlace = (arr) => {
+  for (var i = 0; i <= Math.floor((arr.length - 1) / 2); i++) {
+      let el = arr[i];
+      arr[i] = arr[arr.length - 1 - i];
+      arr[arr.length - 1 - i] = el;
+  }
+  return arr;
+}
